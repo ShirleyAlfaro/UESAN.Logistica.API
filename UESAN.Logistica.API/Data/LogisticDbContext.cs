44924f0970389ce0d3e300b82227481159ce42be
ShirleyK <22200097@ue.edu.pe>
2025-09-19 20:48:20 -0500

    Agregar archivos de proyecto.

---
+++ UESAN.Logistica.API/Data/LogisticDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace UESAN.Logistica.API.Data;
+
+public partial class LogisticDbContext : DbContext
+{
+    public LogisticDbContext()
+    {
+    }
+
+    public LogisticDbContext(DbContextOptions<LogisticDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Products> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-DAI7FU6;Database=LogisticDB;Integrated Security = True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Products>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC0775469585");
+
+            entity.Property(e => e.Description).HasMaxLength(100);
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
